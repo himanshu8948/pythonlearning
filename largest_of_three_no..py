79d953985ff0681c22586ff99638a58a47a6a958
himanshu8948 <himanshupathak894820@gmail.com>
2026-05-24 12:11:35 +0530

    finding largest of three no.

---
+++ largest_of_three_no..py
@@ -0,0 +1,18 @@
+a = int(input("enter first no:"))
+b = int(input("enter second no:"))
+c = int(input("enter third no:"))
+if (a>b and a>c): 
+    
+     print("a is greatest no. of three no.:")
+   
+elif(b>a and b>c):
+      
+      
+     print("b is greatest no. of three no. :")
+
+elif (c>a and c>b):
+    
+    print("c is greatest no. of three no. :")
+
+else:
+     print("no number is greatest in these three no.")
