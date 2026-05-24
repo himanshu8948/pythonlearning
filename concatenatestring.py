4614f802b476abf9438e376afc6b6522bd2d3b7a
himanshu8948 <himanshupathak894820@gmail.com>
2026-05-24 16:21:50 +0530

    concatenate two strings

---
+++ concatenatestring.py
@@ -0,0 +1,6 @@
+string1 = input("Enter your first string :")
+string2 = input("enter your second string :")
+
+concatenate_string = f"{string1} {string2}"
+
+print("your concatenated strings are : ",concatenate_string)
