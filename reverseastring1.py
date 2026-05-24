39f20fdcd373eacc093a66120a86cbf15db5ba89
himanshu8948 <himanshupathak894820@gmail.com>
2026-05-24 14:31:38 +0530

    another method to find the reverse

---
+++ reverseastring1.py
@@ -0,0 +1,8 @@
+string = input("enter the string to reverse it:")
+
+reverse_string = ""
+
+for character in string :
+    reverse_string = character+reverse_string
+
+print("your string got reversed:",reverse_string)
