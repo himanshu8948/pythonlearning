dae9cb54c5f6b7dec773aaf99f39fbf9172fe4e4
himanshu8948 <himanshupathak894820@gmail.com>
2026-05-24 12:22:59 +0530

    checking  a no. if it is positive ,negative or zero

---
+++ positive_negative_zero.py
@@ -0,0 +1,7 @@
+a = float(input("enter a no. to chechk if it is positive,negative,zero:"))
+if(a>0):
+    print("your input value is absolutely positive")
+elif(a== 0):
+    print("your input value is zero!")
+else:
+    print("your input value is 100 % negative value")
