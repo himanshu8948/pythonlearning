dbba1570ffa5c394b87817ee4cf2ea6f8ff70ea8
himanshu8948 <himanshupathak894820@gmail.com>
2026-05-24 13:12:29 +0530

    to print the first and last digit of a string

---
+++ firstandlast.py
@@ -0,0 +1,3 @@
+string  = input("enter your string to get its first character and last character:")
+
+print ("this is the first character of your string:", string [0],"\nthis is your last character:",string [-1]) 
