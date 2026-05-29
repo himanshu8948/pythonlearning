2d28625a1176fc8aa3a34c923028323391709d76
himanshu8948 <himanshupathak894820@gmail.com>
2026-05-29 16:47:08 +0530

    the multiplication of the no.

---
+++ multiplication.py
@@ -0,0 +1,4 @@
+a = int(input("enter the no. to print its multiplication: "))
+
+for i in range(1,11):
+    print(a*i)
---
+++ squareanother.py
@@ -0,0 +1,3 @@
+number = int(input("enter an integer to get its square value :"))
+square = number**2
+print("this is your output integer ")
